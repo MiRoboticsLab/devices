de929e0d7ae8b3ed10558f2be619f73be445548a
wenlinfeng <wenlinfeng@xiaomi.com>
2022-10-12 10:45:48 +0800

    [Modify] Change private uuid and regist joystick handle function
    1.修改自定义服务的uuid
    2.注册摇杆数据的订阅发布
    
    Signed-off-by: wenlinfeng <wenlinfeng@xiaomi.com>

--- cyberdog_bluetooth/cyberdog_bluetooth/cyberdog_bluetooth_main.py
+++ cyberdog_bluetooth/cyberdog_bluetooth/cyberdog_bluetooth_main.py
@@ -14,9 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import bluetooth_node
 import rclpy
 from rclpy.executors import MultiThreadedExecutor
+
+from . import bluetooth_node
 
 
 def main(args=None):
